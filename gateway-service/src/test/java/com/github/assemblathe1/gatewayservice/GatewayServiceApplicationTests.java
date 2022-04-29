45f025888180d8227c0f4beae4dd1a152377f0e4
unknown <taskinroman@mail.ru>
2022-04-29 19:44:38 +0500

    made gateway

---
+++ gateway-service/src/test/java/com/github/assemblathe1/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.assemblathe1.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

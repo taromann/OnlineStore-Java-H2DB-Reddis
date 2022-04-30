9aa1598c0abc5554a4df2a35d6be497da1d58b4f
unknown <taskinroman@mail.ru>
2022-04-30 10:47:42 +0500

    made cart-service

---
+++ cart-service/src/main/java/com/github/assemblathe1/cart/exceptions/CartIsBrokenException.java
@@ -0,0 +1,7 @@
+package com.github.assemblathe1.cart.exceptions;
+
+public class CartIsBrokenException extends RuntimeException {
+    public CartIsBrokenException(String message) {
+        super(message);
+    }
+}
---
+++ cart-service/src/main/java/com/github/assemblathe1/cart/exceptions/ProductServiceIntegrationException.java
@@ -0,0 +1,7 @@
+package com.github.assemblathe1.cart.exceptions;
+
+public class ProductServiceIntegrationException extends RuntimeException {
+    public ProductServiceIntegrationException(String message) {
+        super(message);
+    }
+}
